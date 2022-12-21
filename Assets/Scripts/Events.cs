a238760939aeb8b8497ff283531e8d5b1d8b81f9
fsj <2693900527@qq.com>
2022-12-22 01:12:03 +0800

    feat:增加事件变量

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -2,37 +2,49 @@
 using Photon.Pun;
 using Photon.Realtime;
 
-public static class Events {
+public static class Events
+{
     public const byte F_GAME_START = 0;
-    public const byte F_BASE_HEALTH_CHANGED = 1;
-    public const byte F_BASE_DESTROYED = 2;
-    private const int COUNT_OF_EVENTS = 20;
+    public const byte F_COMPONENT_DESTROYED = 2;
+    public const byte F_COMPONENT_HEALTH_CHANGED = 3;
+    public const byte F_ROBOT_SEIZE_ENEMY = 6;
+    public const byte F_ROBOT_LOST_SEIZE_ENEMY = 7;
+    public const byte M_ROBOT_MOTIVATION_CHANGE = 10;
+    public const byte F_ROBOT_MOTIVATION_CHANGE = 21;
+    public const byte F_BASE_DESTROYED = 36;
+    public const byte F_BASE_HEALTH_CHANGED = 37;
+    private const byte COUNT_OF_EVENTS = 37;
     public delegate void GameEvent(object[] args);
     private static readonly GameEvent[] EVENTS = new GameEvent[COUNT_OF_EVENTS];
 
-    static Events() {
+    static Events()
+    {
         PhotonNetwork.NetworkingClient.EventReceived += NetworkEventCallback;
     }
 
-    private static void NetworkEventCallback(EventData data) {
+    private static void NetworkEventCallback(EventData data)
+    {
         if (data.Code >= COUNT_OF_EVENTS) return;
         if (data.CustomData is object[] args) EVENTS[data.Code]?.Invoke(args);
     }
 
-    public static bool Invoke(byte eventId, object[] args) {
+    public static bool Invoke(byte eventId, object[] args)
+    {
         if (eventId >= COUNT_OF_EVENTS) return false;
         PhotonNetwork.RaiseEvent(eventId, args, RaiseEventOptions.Default, SendOptions.SendReliable);
         EVENTS[eventId]?.Invoke(args);
         return true;
     }
 
-    public static bool AddListener(byte eventId, GameEvent callback) {
+    public static bool AddListener(byte eventId, GameEvent callback)
+    {
         if (eventId >= COUNT_OF_EVENTS) return false;
         EVENTS[eventId] += callback;
         return true;
     }
 
-    public static bool RemoveListener(byte eventId, GameEvent callback) {
+    public static bool RemoveListener(byte eventId, GameEvent callback)
+    {
         if (eventId >= COUNT_OF_EVENTS) return false;
         EVENTS[eventId] -= callback;
         return true;
