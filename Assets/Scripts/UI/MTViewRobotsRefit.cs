e7264e8c59c07ded399f2a9e6130c9a9abcce4c0
yjc <1481497366@qq.com>
2022-12-24 12:14:58 +0800

    feat:优化了装配界面

--- Assets/Scripts/UI/MTViewRobotsRefit.cs
+++ Assets/Scripts/UI/MTViewRobotsRefit.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Model.Equipment;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace UI
 {
@@ -16,7 +18,7 @@ namespace UI
         private const float VIEW_REFIT_PAGE_HEIGHT = 0.8F;
         private const string VIEW_REFIT_PAGE_TITLE = "";
         private Vector2 scroll = Vector2.zero;
-
+        
         private void Start() {
             
         }
@@ -31,9 +33,20 @@ namespace UI
             GUILayout.Window(VIEW_REFIT_PAGE_ID, dim, _ =>
             {
                 GUILayout.BeginHorizontal("Box");
-                
+                GUILayout.Button("ds");
                 GUILayout.BeginVertical();
-                //TODO
+                scroll = GUILayout.BeginScrollView(scroll, false, false,
+                    GUILayout.Height(Screen.height * VIEW_REFIT_PAGE_HEIGHT));
+                foreach (var robot in Summary.team.robots.Values.Where(r => r.status == Robot.STATUS_ACTIVE)) {
+                    GUILayout.BeginHorizontal("Box");
+                    
+                    GUILayout.BeginVertical();
+                    GUILayout.Label(robot.name,GUILayout.ExpandWidth(true));
+                    if (GUILayout.Button("查看机器人", GUILayout.ExpandWidth(false))) {
+                        //TODO
+                    }
+                    GUILayout.EndVertical();
+                }
                 GUILayout.EndVertical();
                 
                 scroll = GUILayout.BeginScrollView(scroll, false, false,
@@ -42,7 +55,10 @@ namespace UI
                     GUILayout.BeginHorizontal("Box");
                     
                     GUILayout.BeginVertical();
-                    GUILayout.Label(component.template.name);
+                    GUILayout.Label(component.template.name,GUILayout.ExpandWidth(true));
+                    if (GUILayout.Button("装配", GUILayout.ExpandWidth(false))) {
+                        //TODO
+                    }
                     GUILayout.EndVertical();
                 }
                 
