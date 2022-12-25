2b776688cd6968855f2ba1a1c5e8b222c4a6a90e
fsj <2693900527@qq.com>
2022-12-25 17:25:06 +0800

    feat:拾取掉落物（部分）

---
+++ Assets/Scripts/System/Dropped/MEAcquireCoins.cs
@@ -0,0 +1,17 @@
+﻿using Photon.Pun;
+
+namespace System {
+    public class MEAcquireCoins : MonoBehaviourPun{
+        private void OnEnable() {
+            Events.AddListener(Events.F_TEAM_ACQUIRE_COINS, AcquireCoins);
+        }
+
+        private void OnDisable() {
+            Events.RemoveListener(Events.F_TEAM_ACQUIRE_COINS, AcquireCoins);
+        }
+
+        public void AcquireCoins(object[] args) {
+            //TODO,在背包里面添加
+        }
+    }
+}
