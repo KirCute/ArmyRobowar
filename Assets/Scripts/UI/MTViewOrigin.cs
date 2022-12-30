426b544c84ca95985a3dcc8c4782de847c3e28d9
yjc <1481497366@qq.com>
2022-12-31 02:28:35 +0800

    feat:完成前期创建房间到开始游戏的工作（除了同步列表）

--- Assets/Scripts/UI/MTViewOrigin.cs
+++ Assets/Scripts/UI/MTViewOrigin.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
@@ -8,10 +9,17 @@ namespace UI
 {
     public class MTViewOrigin : MonoBehaviourPunCallbacks
     {
-        private const int VIEW_START_PAGE_ID = 1;
+        private const int VIEW_ORIGIN_PAGE_ID = 1;
+        private const int VIEW_ORIGIN_PAGE_ID_ = 2;
         private static MTViewOrigin _instance;
-        private const string VIEW_START_PAGE_TITLE = "";
-
+        private const string VIEW_ORIGIN_PAGE_TITLE = "";
+        private string roomNameCreate = "";
+        private string roomNameJoin = "";
+        private GUIStyle text_style = new GUIStyle();
+        private TypedLobby typedLobby;
+        private Dictionary<string, RoomInfo> myRoomList = new Dictionary<string, RoomInfo>();
+        private string roomExistCreate = "";
+        private string roomExistJoin = "";
         public static MTViewOrigin getInstance() {
             return _instance;
         }
@@ -24,35 +32,98 @@ namespace UI
 
         public override void OnConnectedToMaster() {
             base.OnConnectedToMaster();
-            TypedLobby typedLobby = new TypedLobby("customSqlLobby", LobbyType.SqlLobby);//设置当前大厅类型为sqllobby
-            PhotonNetwork.JoinLobby(typedLobby);//加入大厅
-            Debug.Log("加入大厅成功");
+            
         }
 
         private void Start() {
-            PhotonNetwork.ConnectUsingSettings();
+            
         }
 
         private void OnGUI() {
-             GUILayout.Window(VIEW_START_PAGE_ID, new Rect(0,
+            text_style = GUI.skin.textField;
+            text_style.normal.textColor = Color.white;
+            text_style.fontSize = 75;
+             GUILayout.Window(VIEW_ORIGIN_PAGE_ID, new Rect(0,
                     0 ,
-                    Screen.width , Screen.height), _ =>
+                    Screen.width/2 , Screen.height), _ =>
                 {
                     GUILayout.BeginVertical("Box");
-                    
+                    GUILayout.Label(roomExistCreate,GUILayout.ExpandHeight(true));
+                    roomNameCreate=GUILayout.TextField(roomNameCreate,9,text_style,GUILayout.ExpandHeight(true),GUILayout.ExpandWidth(true));
                     GUILayout.Label("",GUILayout.ExpandHeight(true));
                     GUILayout.BeginHorizontal("Box");
                     if (GUILayout.Button("创建房间",GUILayout.ExpandHeight(false),GUILayout.Height(Screen.height/6))) {
-                        //TODO
-                    }
-                    if (GUILayout.Button("加入房间",GUILayout.ExpandHeight(false),GUILayout.Height(Screen.height/6))); {
-                        //TODO
+                        if (roomNameCreate.Length>2) {
+                            if (myRoomList.ContainsKey(roomNameCreate)) {
+                                roomExistCreate = "房间已存在";
+                            }
+                            else {
+                                roomExistCreate = "";
+                                RoomOptions roomOptions = new RoomOptions { MaxPlayers = 10 };
+                                PhotonNetwork.CreateRoom(roomNameCreate, roomOptions);
+                                _instance.enabled = false;
+                                MTViewStart.getInstance().enabled = true;
+                            }
+                        }
                     }
                     GUILayout.EndHorizontal();
                     GUILayout.EndVertical();
                 },
-                VIEW_START_PAGE_TITLE);
+                 VIEW_ORIGIN_PAGE_TITLE);
+             GUILayout.Window(VIEW_ORIGIN_PAGE_ID_, new Rect(Screen.width/2,
+                     0 ,
+                     Screen.width/2 , Screen.height), _ =>
+                 {
+                     GUILayout.BeginVertical("Box");
+                     GUILayout.Label(roomExistJoin,GUILayout.ExpandHeight(true));
+                     roomNameJoin=GUILayout.TextField(roomNameJoin,9,text_style,GUILayout.ExpandHeight(true),GUILayout.ExpandWidth(true));
+                     GUILayout.Label("",GUILayout.ExpandHeight(true));
+                     GUILayout.BeginHorizontal("Box");
+                     if (GUILayout.Button("加入房间",GUILayout.ExpandHeight(false),GUILayout.Height(Screen.height/6))); {
+                         if (roomNameJoin.Length>2) {
+                             if (myRoomList.ContainsKey(roomNameJoin)) {
+                                 roomExistJoin = "";
+                                 PhotonNetwork.JoinRoom(roomNameJoin);
+                                 _instance.enabled = false;
+                                 MTViewStart.getInstance().enabled = true;
+                             }
+                             else {
+                                 roomExistJoin = "不存在此房间";
+                             }
+                         }
+                     }
+                     GUILayout.EndHorizontal();
+                     GUILayout.EndVertical();
+                 },
+                 VIEW_ORIGIN_PAGE_TITLE);
         }
-        
+
+        public override void OnRoomListUpdate(List<RoomInfo> roomList) {
+            base.OnRoomListUpdate(roomList);
+
+            //遍历房间信息改变了的房间，注意这个roomList不包括所有房间，而是有属性改变了的房间，比如新增的
+            foreach (var r in roomList)
+            {
+                //清除关闭或不可显示或已经移除了的房间
+                if (!r.IsOpen || !r.IsVisible || r.RemovedFromList)
+                {
+                    if (myRoomList.ContainsKey(r.Name))//如果该房间之前有，现在没有了就去掉它
+                    {
+                        myRoomList.Remove(r.Name);
+                    }
+                    continue;
+                }
+                //更新房间信息
+                if (myRoomList.ContainsKey(r.Name))
+                {
+                    myRoomList[r.Name] = r;
+                }
+                //添加新房间
+                else
+                {
+                    myRoomList.Add(r.Name, r);//如果该房间之前没有，现在有了就加进myRoomList
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/UI/MTViewStart.cs
+++ Assets/Scripts/UI/MTViewStart.cs
@@ -3,22 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using Photon.Pun;
 using Photon.Realtime;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using Random = System.Random;
 
 namespace UI
 {
-    public class MTViewStart : MonoBehaviour
+    public class MTViewStart : MonoBehaviourPunCallbacks
     {
+        private static MTViewStart _instance;
         private const int VIEW_START_PAGE_ID = 0;
 
         private const string VIEW_START_PAGE_TITLE = "";
         
         private readonly GUIStyle style = new(); //定义控件
 
-        private readonly List<Player> tempTestRed = new();
-        private readonly List<Player> tempTestBlue = new();
+        private readonly List<Player> tempTestRed = new();//1
+        private readonly List<Player> tempTestBlue = new();//0
         private readonly Dictionary<Player, bool> ready = new();
+        private static Random _random = new Random();
+        private int blueHome;
+        private int redHome;
         private int myTeam;
+
+        public static MTViewStart getInstance() {
+            return _instance;
+        }
+        private void Awake() {
+            _instance = this;
+            do {
+                blueHome = _random.Next(0, 5);
+                redHome = _random.Next(0, 5);
+            } while (blueHome == redHome);
+        }
 
         private void OnEnable() {
             Events.AddListener(Events.M_PLAYER_ATTEND, OnPlayerAttend);
@@ -47,20 +64,81 @@ namespace UI
                 {
                     GUILayout.BeginVertical("Box");
                     GUILayout.BeginHorizontal("Box");
-                    if (!ready.ContainsKey(PhotonNetwork.LocalPlayer) || !ready[PhotonNetwork.LocalPlayer]) //按下准备后切换按钮，if里面放的是个人是否准备
-                    {
-                        if (GUILayout.Button("准备游戏", style))
+                    if (PhotonNetwork.IsMasterClient) {
+                        if (GUILayout.Button("开始游戏", style)) {
+                            if (IsAllReady()) {
+                                var input = new object[5 + tempTestBlue.Count + tempTestRed.Count];
+                                input[0] = PhotonNetwork.Time;
+                                input[1] = blueHome;
+                                input[2] = redHome;
+                                input[3] = tempTestBlue.Count;
+                                input[4] = tempTestBlue[0];
+                                int cntBlue = 1;
+                                for (int i = 5; i < 5+tempTestBlue.Count-1; i++) {
+                                    input[i] = tempTestBlue[cntBlue];
+                                    cntBlue++;
+                                }
+                                input[3 + tempTestBlue.Count] = tempTestRed.Count;
+                                input[3 + tempTestBlue.Count + 1] = tempTestBlue[0];
+                                int cntRed = 1;
+                                for (int i = 3 + tempTestBlue.Count + 1 + 1; i < 5 + tempTestBlue.Count + tempTestRed.Count; i++) {
+                                    input[i] = tempTestRed[cntRed];
+                                    cntRed++;
+                                }
+                                Events.Invoke(Events.F_GAME_START, input);
+                            }
+                        }
+                    }
+                    else{
+                        if (!ready.ContainsKey(PhotonNetwork.LocalPlayer) ||
+                            !ready[PhotonNetwork.LocalPlayer]) //按下准备后切换按钮，if里面放的是个人是否准备
                         {
-                            Events.Invoke(Events.M_PLAYER_READY, new object[] {PhotonNetwork.LocalPlayer, true});
+                            if (GUILayout.Button("准备游戏", style)) {
+                                Events.Invoke(Events.M_PLAYER_READY, new object[] { PhotonNetwork.LocalPlayer, true });
+                            }
+                        }
+                        else {
+                            if (GUILayout.Button("取消准备", style)) {
+                                Events.Invoke(Events.M_PLAYER_READY, new object[] { PhotonNetwork.LocalPlayer, false });
+                            }
+                        }
+                    }
+                    if (myTeam == 0) {
+                        if (tempTestRed.Count == 5) {
+                            if (GUILayout.Button("队伍已满",style)) {}
+                        }
+                        else {
+                            if (GUILayout.Button("交换队伍",style)) {
+                                if (myTeam == 0) {
+                                    if (tempTestRed.Count == 5) {
+                                
+                                    }
+                                }
+                                Events.Invoke(Events.M_CHANGE_TEAM, new object[] {PhotonNetwork.LocalPlayer, 1 - myTeam});
+                            }
                         }
                     }
                     else {
-                        if (GUILayout.Button("取消准备", style)) {
-                            Events.Invoke(Events.M_PLAYER_READY, new object[] {PhotonNetwork.LocalPlayer, false});
+                        if (tempTestBlue.Count == 5) {
+                            if (GUILayout.Button("队伍已满",style)) {}
+                        }
+                        else {
+                            if (GUILayout.Button("交换队伍",style)) {
+                                if (myTeam == 0) {
+                                    if (tempTestRed.Count == 5) {
+                                
+                                    }
+                                }
+                                Events.Invoke(Events.M_CHANGE_TEAM, new object[] {PhotonNetwork.LocalPlayer, 1 - myTeam});
+                            }
                         }
                     }
-
                     if (GUILayout.Button("交换队伍",style)) {
+                        if (myTeam == 0) {
+                            if (tempTestRed.Count == 5) {
+                                
+                            }
+                        }
                         Events.Invoke(Events.M_CHANGE_TEAM, new object[] {PhotonNetwork.LocalPlayer, 1 - myTeam});
                     }
 
@@ -71,19 +149,49 @@ namespace UI
                     GUILayout.BeginHorizontal();
                     
                     GUILayout.BeginVertical("Box");
-                    GUILayout.Box("red1",style,GUILayout.ExpandHeight(true));
-                    GUILayout.Box("red2",style,GUILayout.ExpandHeight(true));
-                    GUILayout.Box("red3",style,GUILayout.ExpandHeight(true));
-                    GUILayout.Box("red4",style,GUILayout.ExpandHeight(true));
-                    GUILayout.Box("red5",style,GUILayout.ExpandHeight(true));
+                    foreach (var player in tempTestBlue) {
+                        if (ready[player]) {
+                            GUIStyle styleTemp = new GUIStyle();
+                            styleTemp.alignment = TextAnchor.MiddleCenter;
+                            styleTemp.fontSize = 24;
+                            styleTemp.normal.textColor = Color.green;
+                            GUILayout.Box(player.NickName,styleTemp,GUILayout.ExpandHeight(true));
+                        }
+                        else {
+                            GUILayout.Box(player.NickName,style,GUILayout.ExpandHeight(true));
+                        }
+                    }
+
+                    for (int i = 0; i < 5-tempTestBlue.Count; i++) {
+                        GUIStyle styleTemp = new GUIStyle();
+                        styleTemp.alignment = TextAnchor.MiddleCenter;
+                        styleTemp.fontSize = 24;
+                        styleTemp.normal.textColor = Color.magenta;
+                        GUILayout.Box("NULL",styleTemp,GUILayout.ExpandHeight(true));
+                    }
                     GUILayout.EndVertical();
                     
                     GUILayout.BeginVertical("Box");
-                    GUILayout.Box("blue1",style,GUILayout.ExpandHeight(true));
-                    GUILayout.Box("blue2",style,GUILayout.ExpandHeight(true));
-                    GUILayout.Box("blue3",style,GUILayout.ExpandHeight(true));
-                    GUILayout.Box("blue4",style,GUILayout.ExpandHeight(true));
-                    GUILayout.Box("blue5",style,GUILayout.ExpandHeight(true));
+                    foreach (var player in tempTestRed) {
+                        if (ready[player]) {
+                            GUIStyle styleTemp = new GUIStyle();
+                            styleTemp.alignment = TextAnchor.MiddleCenter;
+                            styleTemp.fontSize = 24;
+                            styleTemp.normal.textColor = Color.green;
+                            GUILayout.Box(player.NickName,styleTemp,GUILayout.ExpandHeight(true));
+                        }
+                        else {
+                            GUILayout.Box(player.NickName,style,GUILayout.ExpandHeight(true));
+                        }
+                    }
+
+                    for (int i = 0; i < 5-tempTestRed.Count; i++) {
+                        GUIStyle styleTemp = new GUIStyle();
+                        styleTemp.alignment = TextAnchor.MiddleCenter;
+                        styleTemp.fontSize = 24;
+                        styleTemp.normal.textColor = Color.magenta;
+                        GUILayout.Box("NULL",styleTemp,GUILayout.ExpandHeight(true));
+                    }
                     GUILayout.EndVertical();
                     
                     GUILayout.EndHorizontal();
@@ -93,12 +201,37 @@ namespace UI
         }
 
         private void OnPlayerAttend(object[] args) {
+            if (PhotonNetwork.IsMasterClient) Events.Invoke(Events.M_PLAYER_READY, new object[] {PhotonNetwork.LocalPlayer, true});
             var player = (Player) args[0];
             ready.Add(player, false);
-            if (PhotonNetwork.IsMasterClient) {
-                var team = 0;  // TODO: 决定新玩家进入哪个队
+        }
+
+        public override void OnJoinedRoom() {
+            base.OnJoinedRoom();
+            var player = PhotonNetwork.LocalPlayer;
+            if(!PhotonNetwork.IsMasterClient){
+                Events.Invoke(Events.M_PLAYER_ATTEND, new object[] { player });
+            }
+            if (tempTestBlue.Count == 0 && tempTestRed.Count == 0) {
+                var team = 0;  
+                Events.Invoke(Events.M_CHANGE_TEAM, new object[] {player, team});
+            }else if (tempTestBlue.Count==5&&tempTestRed.Count<5) {
+                var team = 1;  
+                Events.Invoke(Events.M_CHANGE_TEAM, new object[] {player, team});
+            }else if (tempTestRed.Count==5&&tempTestBlue.Count<5) {
+                var team = 0;  
+                Events.Invoke(Events.M_CHANGE_TEAM, new object[] {player, team});
+            }else if (tempTestBlue.Count==tempTestRed.Count) {
+                var team = 0;  
+                Events.Invoke(Events.M_CHANGE_TEAM, new object[] {player, team});
+            }else if (tempTestBlue.Count > tempTestRed.Count) {
+                var team = 1;  
+                Events.Invoke(Events.M_CHANGE_TEAM, new object[] {player, team});
+            }else {
+                var team = 0;  
                 Events.Invoke(Events.M_CHANGE_TEAM, new object[] {player, team});
             }
+        
         }
 
         private void OnPlayerReady(object[] args) {
