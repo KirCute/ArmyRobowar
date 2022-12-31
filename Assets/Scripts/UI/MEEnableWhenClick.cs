c43ca525986332256452b2d71c7f3fcaa25c43ac
KirCute <951206789@qq.com>
2022-12-31 17:08:42 +0800

    feat: 布设操作大厅

---
+++ Assets/Scripts/UI/MEEnableWhenClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace UI {
+    public class MEEnableWhenClick : MonoBehaviour {
+        [SerializeField] private MonoBehaviour managingScript;
+
+        public void OnClick() {
+            managingScript.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MEMainCameraController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI {
+    public class MEMainCameraController : MonoBehaviour {
+        [SerializeField] private Camera mainCamera;
+        [SerializeField] private Vector3 mainCameraInitPosition;
+        [SerializeField] private Vector3 mainCameraInitRotation;
+
+        private void Start() {
+            mainCamera.transform.position = mainCameraInitPosition;
+            mainCamera.transform.eulerAngles = mainCameraInitRotation;
+        }
+
+        private void Update() {
+            
+        }
+    }
+}
