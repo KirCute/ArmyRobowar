ab00330ca51be4e7197052d4680276a2b533d82e
fsj <2693900527@qq.com>
2023-01-01 21:35:27 +0800

    feat:标靶准星

---
+++ Assets/Scripts/UI/MTViewForesight.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Unity.VisualStudio.Editor;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace UI {
+    public class MTViewForesight : MonoBehaviour{
+        private GameObject foresightObject;
+        private Image foresight;//标靶准星
+
+        private void Awake() {
+            foresightObject = transform.Find("Foresight").gameObject;
+            foresight = foresightObject.GetComponent<Image>();
+            foresightObject.SetActive(false);
+        }
+
+        private void OnEnable() {
+            Events.AddListener(Events.M_ROBOT_MONITOR, OnMonitor);
+        }
+
+        private void OnDisable() {
+            Events.RemoveListener(Events.M_ROBOT_MONITOR, OnMonitor);
+        }
+
+        private void OnMonitor(object[] args) {
+            if (Equals((Player) args[1], PhotonNetwork.LocalPlayer)) {
+                if ((bool) args[2]) {
+                    foresightObject.SetActive(true);
+                } else {
+                    foresightObject.SetActive(false);
+                }
+            }
+        }
+        
+    }
+}
